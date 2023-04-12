bc6dea3095fcd890f6a9217948232fe71503a8fd
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-04-12 12:55:23 +0200

    Excercise 1 done

---
+++ Nivell-1/index.php
@@ -0,0 +1,17 @@
+<?php
+
+//Exercici 1
+$varInteger = 10;
+$varDouble = 2.5;
+$varString = "Gabriela";
+$varBoolean = true;
+
+echo "Integer:  ". $varInteger."<br/>";
+echo "Double:  ". $varDouble."<br/>";
+echo "String:   ". $varString."<br/>";
+echo "Boolean:   ".$varBoolean."<br/>";
+
+$resBool = ($varBoolean == "1")?"true":"false";
+echo "Boolean:   ".$resBool."<br/>";
+
+?>
