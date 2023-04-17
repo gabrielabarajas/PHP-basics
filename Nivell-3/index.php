83240f09411b956215d09d9fa51754e3c1c102ea
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-04-17 12:13:59 +0200

    Nivell 3 Excercici 2 completed

--- Nivell-3/index.php
+++ Nivell-3/index.php
@@ -10,9 +10,22 @@ function stringToArray($str1){
 $uno = "Hello World";
 $array = stringToArray($uno);
 print_r($array);
-echo "\n";
+echo "<br/>";
 
 $uno = "World    H    e       ll   o";
 $array = stringToArray($uno);
 print_r($array);
+echo "<br/>";
+
+// Excercici 2
+function countTimesInArray($arr1){
+    $resArray = array_count_values($arr1);
+    return $resArray; 
+}
+
+// Testing
+$unoArray = array("uno","dos","tres", 1,3,"tres","dos","tres", 1,2,);
+$countArray = countTimesInArray($unoArray);
+print_r($countArray);
+echo "<br/>";
 ?>
