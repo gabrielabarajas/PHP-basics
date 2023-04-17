292dc0d53faf73b8d91e5a2b19ebfdf25d0227af
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-04-17 10:55:09 +0200

    Nivell 2 Excercici 2 Completed

--- Nivell-2/index.php
+++ Nivell-2/index.php
@@ -21,5 +21,45 @@ echo addInt($uno,$dos)."<br/>";
 
 $uno=2;
 $dos= 2;
-echo addInt($uno,$dos);
+echo addInt($uno,$dos)."<br/>";
+
+//Exercici 2
+
+// function charExchange($str1){
+//     if (strlen($str1) == 0){
+//         echo "Cannot execute function: Empty string.";
+//     }else{
+//         $first = substr($str1,1);
+//         $last = substr($str1,-1);
+//         $str1[0] = $last;
+//         $str1[strlen($str1)-1] = $first;
+//         echo $str1."\n";
+//     }
+// }
+
+function charExchangeAgain($str1){
+    if (strlen($str1) == 0){
+        echo "Cannot execute function: Empty string." ."<br/>";
+    }else{
+        $first = $str1[0];
+        $last = $str1[strlen($str1)-1];
+        $str1[0] = $last;
+        $str1[strlen($str1)-1] = $first;
+        echo $str1."<br/>";
+    }
+}
+
+//Testing cases
+$onestr = "wxyz";
+charExchangeAgain($onestr);
+
+$onestr = "";
+charExchangeAgain($onestr);
+
+$onestr = "a";
+charExchangeAgain($onestr);
+
+$onestr = "ab";
+charExchangeAgain($onestr);
+
 ?>
