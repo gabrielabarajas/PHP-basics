0fe787fd452263cbb66c94c279695b9f4e95c6ad
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-04-18 09:35:00 +0200

    Nivell-3 , Excercici 3 Completed

--- Nivell-3/index.php
+++ Nivell-3/index.php
@@ -28,4 +28,25 @@ $unoArray = array("uno","dos","tres", 1,3,"tres","dos","tres", 1,2,);
 $countArray = countTimesInArray($unoArray);
 print_r($countArray);
 echo "<br/>";
+
+//Excercici 3
+function deleteElement($arrayone, $element){
+    unset($arrayone["$element"]);
+    return $arrayone;
+}
+function normalizeArray($arr1){
+    array_splice($arr1,1,1);
+    return $arr1;
+}
+
+//Testing
+$arrayTest = array(10,20,30,40,50);
+print_r($arrayTest);
+echo "<br/>";
+$arrTestDelete = deleteElement($arrayTest,3);
+print_r($arrTestDelete);
+echo "<br/>";
+$arrTestDelete = normalizeArray($arrayTest,1,1);
+print_r($arrTestDelete);
+
 ?>
