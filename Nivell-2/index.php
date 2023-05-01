c15cffee7a7c612ad23c4f566808cba3738cf0d2
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-05-01 21:00:16 +0200

    changes made from comments fron teacher Nivell 2 Excercise 1

--- Nivell-2/index.php
+++ Nivell-2/index.php
@@ -11,9 +11,6 @@ function addInt($num1, $num2){
 }
 
 //Testing cases
-$uno=1;
-$dos= 2;
-echo addInt($uno,$dos)."<br/>";
 
 $uno=3;
 $dos= 2;
@@ -22,6 +19,25 @@ echo addInt($uno,$dos)."<br/>";
 $uno=2;
 $dos= 2;
 echo addInt($uno,$dos)."<br/>";
+
+function addIntRefact($num1, $num2){
+    $res = $num1+$num2;
+    if ($num1 === $num2){
+        $res = 2*$res;
+    }
+    return $res;
+}
+
+//Testing cases
+
+
+$uno=3;
+$dos= 2;
+echo addIntRefact($uno,$dos)."<br/>";
+
+$uno=2;
+$dos= 2;
+echo addIntRefact($uno,$dos)."<br/>";
 
 //Exercici 2
 
